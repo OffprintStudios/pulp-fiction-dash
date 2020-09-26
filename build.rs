0a03784185a35795c6006e0f14ee10fc80ebac61
Alyx Mote <figments@outlook.com>
2020-09-25 18:09:57 -0700

    fixed build script for rust

--- build.rs
+++ build.rs
@@ -1,17 +1,19 @@
 use std::process::Command;
 
 fn main() {
-    let outputDeps = Command::new("yarn")
-        .args($["--cwd", "./client", "install"])
+    let output_deps = Command::new("yarn")
+        .args(&["--cwd", "./client", "install"])
         .output()
         .expect("To fetch client dependencies");
 
-    let outputBuild = Command::new("yarn")
+    let output_build = Command::new("yarn")
         .args(&["--cwd", "./client", "run", "build", "--prod"])
         .output()
         .expect("To build the client successfully");
 
-    if !outputDeps.status.success() && !outputBuild.status.success() {
-        panic!("Error while compiling:\n{}", String::from_utf8_lossy(&output.stdout));
+    if !output_deps.status.success() {
+        panic!("Error while compiling:\n{}", String::from_utf8_lossy(&output_deps.stdout));
+    } else if !output_build.status.success() {
+        panic!("Error while compiling:\n{}", String::from_utf8_lossy(&output_build.stdout));
     }
 }
