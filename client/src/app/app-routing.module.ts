f15d13031e264e3ffe7c0004b3137b0f69d1662c
Alyx Mote <figments@outlook.com>
2020-10-04 01:45:04 -0700

    added routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AuditComponent } from './pages/audit';
+import { DocsComponent } from './pages/docs';
+import { HomeComponent } from './pages/home';
+import { NewsComponent } from './pages/news';
+import { QueueComponent } from './pages/queue';
+import { ReportsComponent } from './pages/reports';
+import { UsersComponent } from './pages/users';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'queue', component: QueueComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'docs', component: DocsComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'audit', component: AuditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
